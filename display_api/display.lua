4d483563f67cf1d45d570fe25fcc8252953804bf
flux <25628292+fluxionary@users.noreply.github.com>
2022-10-26 10:23:06 -0700

    don't let explosions destroy sign entities

--- display_api/display.lua
+++ display_api/display.lua
@@ -81,9 +81,16 @@ end
 -- Detect rotation restriction
 local rotation_restricted = nil
 minetest.register_entity('display_api:dummy_entity', {
-	collisionbox = { 0, 0, 0, 0, 0, 0 },
-	visual = "upright_sprite",
-	textures = {} })
+	initial_properties = {
+		collisionbox = {0, 0, 0, 0, 0, 0},
+		visual = "upright_sprite",
+		is_visible = false,
+		textures = {"blank.png"}
+	},
+	on_blast = function(self, damage)
+		return false, false, {}
+	end,
+})
 
 function display_api.is_rotation_restricted()
 	if rotation_restricted == nil then
@@ -304,12 +311,17 @@ end
 function display_api.register_display_entity(entity_name)
 	if not minetest.registered_entities[entity_name] then
 		minetest.register_entity(':'..entity_name, {
-			collisionbox = { 0, 0, 0, 0, 0, 0 },
-			visual = "upright_sprite",
-			textures = {},
+			initial_properties = {
+				collisionbox = {0, 0, 0, 0, 0, 0},
+				visual = "upright_sprite",
+				textures = {},
+			},
 			on_activate = display_api.on_activate,
 			get_staticdata = function(self)
 				return minetest.serialize({ nodepos = self.nodepos })
+			end,
+			on_blast = function(self, damage)
+				return false, false, {}
 			end,
 		})
 	end
